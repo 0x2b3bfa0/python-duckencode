8637bd0a367f58ccec60dd085d3370fc50d2f8af
Helio <crushedice2000@gmail.com>
2015-10-24 18:00:54 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 Description:
 ------------
 
-This program converts Duckyscript code into Arduino code. This arduino code will only work on AVR chips with USB support.
+This program converts Duckyscript code into Arduino code. This arduino code will only work on AVR chips with USB support. I was inspired by [this project][1]
 
 Installation:
 -------------
 
 1. Clone this repository and `cd` into it:
- 
+
         git clone https://github.com/crushedice2000/python-duckencode.git
         cd python-duckencode
 
 2. Make public the `sendReport()` function:
 
     You should edit a file called `USBAPI.h`. This file may be on one of the following paths<sup>[1](#footnote_1)</sup>:
-    
+
         {ARDUINO}/hardware/arduino/cores/arduino/USBAPI.h
         {SKETCHES}/hardware/arduino/cores/arduino/USBAPI.h
-        
-    
+
+
     Once you've found this file, open it with your favorite text editor and locate these lines:
-    
+
         private:
             KeyMap* _keyMap;
             void sendReport(KeyReport* keys);
@@ -30,11 +30,11 @@ Installation:
             virtual size_t write(uint8_t);
 
     Simply cut this line from the `private` section and paste it on the `public` section:
-    
+
         void sendReport(KeyReport* keys);
 
     Once edited these lines should look like this:
-    
+
         private:
             KeyMap* _keyMap;
             void setKeyMap(KeyMap* keyMap);
@@ -47,9 +47,8 @@ Installation:
 
         ./compiler.py
 
+---
 
+<a name="footnote_1">**1**</a>**:** `{ARDUINO}` represents the directory into which the Arduino IDE and supporting files have been installed. This may be `/usr/local/arduino` or `/usr/share/arduino` or one of many other possible choices depending on your operating system.
 
-https://ctrlaltnarwhal.wordpress.com/2012/10/31/installing-usb-rubber-ducky-on-3rd-party-devices/
-http://codereview.stackexchange.com/questions/108174/duckyscript-precompiler-for-arduino-leonardo http://arduino.stackexchange.com/questions/17057/keyboard-print-skips-keys
- 
-<a name="footnote_1">Footnote 1</a>: `{ARDUINO}` represents the directory into which the Arduino IDE and supporting files have been installed. This may be `/usr/local/arduino` or `/usr/share/arduino` or one of many other possible choices depending on your operating system.
+[1]: https://ctrlaltnarwhal.wordpress.com/2012/10/31/installing-usb-rubber-ducky-on-3rd-party-devices/
--- compiler.py
+++ compiler.py
@@ -19,10 +19,10 @@ from data import code
 
 delay = 0
 cmdtype = 0
-commands = ""
-includes = ""
-defines = ""
-loop = ""
+commands = []
+includes = []
+defines = []
+loop = []
 
 
 def info(type, msg, **kwargs):
@@ -52,7 +52,7 @@ def getkey(keys):
         try:
             key = [keycodes.get(code) for code in keycodes if key in code][0]
         except IndexError:
-            info(0, 'unrecognized key: {}'.format(key), exit=127)
+            info(0, 'unrecognized key: {}'.format(key), exit=5)
 
         if key[1]:
             modifier_keys |= key[0]
@@ -60,16 +60,18 @@ def getkey(keys):
             normal_keys.append(key[0])
 
     if len(normal_keys) > 6:
-        info(0, 'maximum number of non-modifier keys per line is 6', exit=126)
+        info(0, 'maximum number of non-modifier keys per line is 6', exit=4)
 
-    for index in range(0,6):
-        arguments.append(normal_keys.get(index, 0))
+    for index in range(0, 6):
+        try:
+            arguments.append(normal_keys[index])
+        except:
+            arguments.append(0)
+
     arguments.append(modifier_keys)
-
     arguments = [format(byte, '#04x') for byte in arguments]
 
-    commands += 'sendKey({},{},{},{},{},{},{});'.format(*arguments)
-    commands += '\n'
+    commands.append('sendKey({},{},{},{},{},{},{});'.format(*arguments))
 
     return True
 
@@ -104,91 +106,86 @@ for line in fileinput.input([input_file]):
         command = command.upper()
 
     if command in ('REM', 'COMMENT', '#', '//', ';'):
-        commands += '// {}'.format(options)
-        commands += '\n'
+        commands.append('// {}'.format(options))
         needs_delay = False
         cmdtype = -1
 
     elif command in ('INCLUDE'):
         info(3, 'don\'t use {} unless you really need it'.format(command))
         if cmdtype > 0:
-            info(0, 'you shouldn\'t mix {} in the code'.format(command), exit=5)
-        includes += '#include {}'.format(options)
-        includes += '\n'
+            info(0, 'you shouldn\'t mix {} in the code'.format(command), exit=1)
+        includes.append('#include {}'.format(options))
         needs_delay = False
         cmdtype = 0
 
     elif command in ('DEFINE'):
         info(3, 'don\'t use {} unless you really need it'.format(command))
         if cmdtype > 1:
-            info(0, 'you shouldn\'t mix {} in the code'.format(command), exit=5)
-        includes += '#define {}'.format(options)
-        includes += '\n'
+            info(0, 'you shouldn\'t mix {} in the code'.format(command), exit=1)
+        defines.append('#define {}'.format(options))
         needs_delay = False
         cmdtype = 1
 
     elif command in ('LOOP', '@'):
         info(3, "don't use {} unless you really need it".format(command))
         if cmdtype > 2:
-            info(0, 'you should put {} in the top of the program'.format(command), exit=5)
-        loop += options
-        loop += '\n'
+            info(0, 'you should put {} in the top of the program'.format(command), exit=1)
+        loop.append(options)
         needs_delay = False
         cmdtype = 2
 
     elif command in ('ARDUINO', 'CODE', '%'):
         info(3, "don't use {} unless you really need it".format(command))
-        commands += options
-        commands += '\n'
+        commands.append(options)
         needs_delay = False
         cmdtype = 3
 
     elif command in ('DEFAULT_DELAY', 'DEFAULTDELAY'):
         if not options.isdigit():
-            info(0, '{} only accepts integers'.format(command), exit=11)
+            info(0, '{} only accepts integers'.format(command), exit=3)
         delay = int(options)
         needs_delay = False
         cmdtype = -1
 
     elif command in ('SLEEP', 'DELAY', 'WAIT'):
         if not options.isdigit():
-            info(0, '{} only accepts integers'.format(command), exit=11)
-        commands += 'delay({});'.format(options)
-        commands += '\n'
+            info(0, '{} only accepts integers'.format(command), exit=3)
+        commands.append('delay({});'.format(options))
         needs_delay = False
         cmdtype = 3
 
     elif command in ('REPEAT'):
         if not options.isdigit():
-            info(0, '{} only accepts integers'.format(command), exit=11)
+            info(0, '{} only accepts integers'.format(command), exit=3)
         last_command = commands.splitlines()[-1]
-        commands += repeat.format(options, last_command, delay)
+        commands.append(repeat.format(options, last_command, delay))
         needs_delay = False
         cmdtype = 3
 
     elif command in ('STRING', 'TEXT', 'PRINT'):
-        commands += 'Keyboard.print("{}");'.format(options.replace('"', '\\"'))
-        commands += '\n'
+        commands.append('Keyboard.print("{}");'.format(options.replace('"', '\\"')))
         needs_delay = True
         cmdtype = 3
 
     else:
         cmdtype = 3
         if not getkey(line):
-            info(0, 'unrecognized command: {}'.format(command), exit=10)
+            info(0, 'unrecognized command: {}'.format(command), exit=2)
 
     if delay > 0 and needs_delay:
-        commands += 'delay({});'.format(delay)
-        commands += '\n'
+        commands.append('delay({});'.format(delay))
 
-commands = "\n".join(["  " + i for i in commands.splitlines()])
-loop = "\n".join(["  " + i for i in loop.splitlines()])
+commands = ['  ' + line for line in commands]
+loop = ['  ' + line for line in loop]
 
-code = code.format(includes=includes,
-                   defines=defines,
-                   setup=commands,
-                   loop=loop)
+code = code.format(includes='\n'.join(defines),
+                   defines='\n'.join(defines),
+                   setup='\n'.join(commands),
+                   loop='\n'.join(loop))
 code = code.strip()
 
-with open(output_file, 'w') as output:
-    output.write(code)
+try:
+    with open(output_file, 'w') as output:
+        output.write(code)
+except Exception as e:
+    info(0, '{}'.format(e), exit=6)
